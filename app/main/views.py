e1457f4270f491e9e0320e4f4077a5814c516be6
Robert Maina <robert.maina@student.moringaschool.com>
2021-08-24 12:17:32 +0300

    created auth files

---
+++ app/main/views.py
@@ -0,0 +1,25 @@
+from app.models import User, Blog, Comment
+from flask import render_template, abort
+from . import main
+from flask_login import login_required, current_user
+
+@main.route('/')
+def index():
+
+    title= 'Welcome to Kreative Brands'
+    return render_template('index.html', title = title)
+
+@main.route('/about')
+def about():
+    tittle = 'About Kreative Brands'
+    return render_template('about.html', tittle = tittle)
+
+@main.route('/user/<name>')
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    user_id = current_user._get_current_object().id
+    message = Blog.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,posts=message)
---
+++ app/models.py
@@ -0,0 +1,84 @@
+from flask_login import login_manager, login_user, UserMixin
+from . import db, login_manager
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email  = db.Column(db.String(255),unique = True,nullable = False)
+    user_password = db.Column(db.String(255),nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    
+
+    
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.user_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.user_password,password) 
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class Product(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255),nullable = False)
+    price = db.Column(db.Text(), nullable = False)
+    category = db.Column(db.String(255), index = True,nullable = False)
+
+    comment = db.relationship('Comment',backref='blog',lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+        
+    def __repr__(self):
+        return f'Blog {self.post}'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'),nullable = False)
+
+    def add_coment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,blog_id):
+        comments = Comment.query.filter_by(blog_id=blog_id).all()
+
+        return comments
+
+    def __repr__(self):
+        return f'comment:{self.comment}'
